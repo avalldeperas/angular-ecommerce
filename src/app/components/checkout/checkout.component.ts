4518649acb7840ade1647a25c59b8b70eea91776
avalldeperas <albertvalldeperas@gmail.com>
2020-06-23 17:19:49 +0200

    adds billingAddress checkbox to copy shippingAddr and adds the rest of form groups

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -10,24 +10,59 @@ import {first} from 'rxjs/operators';
 export class CheckoutComponent implements OnInit {
 
   checkoutFormGroup: FormGroup;
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-    this.handleFormGroup();
+    this.initFormGroups();
   }
 
-  private handleFormGroup() {
-    this.checkoutFormGroup = new FormGroup({
-      customer: new FormGroup({
-        firstName: new FormControl(),
-        lastName: new FormControl()
+  private initFormGroups() {
+    this.checkoutFormGroup = this.formBuilder.group({
+      customer: this.formBuilder.group({
+        firstName: [''],
+        lastName: [''],
+        email: [''],
+      }),
+      shippingAddress: this.formBuilder.group({
+        country: [''],
+        street: [''],
+        city: [''],
+        state: [''],
+        zipCode: [''],
+      }),
+      billingAddress: this.formBuilder.group({
+        country: [''],
+        street: [''],
+        city: [''],
+        state: [''],
+        zipCode: [''],
+      }),
+      creditCard: this.formBuilder.group({
+        cardType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: [''],
       })
     });
   }
 
   onSubmit() {
     const customerFormValues = this.checkoutFormGroup.get('customer').value;
-    console.log("firstName = ", customerFormValues);
+    console.log("customerFormValues: ", customerFormValues);
+    const shippingFormValues = this.checkoutFormGroup.get('shippingAddress').value;
+    console.log("customerFormValues: ", shippingFormValues);
+    const creditCardFormValues = this.checkoutFormGroup.get('creditCard').value;
+    console.log("customerFormValues: ", creditCardFormValues);
+  }
+
+  copyShippingToBillingAddress(event) {
+    event.target.checked ?
+      this.checkoutFormGroup.controls.billingAddress.setValue(this.checkoutFormGroup.controls.shippingAddress.value)
+      : this.checkoutFormGroup.controls.billingAddress.reset();
   }
 }
